ffbb7bd8dd7f2ad88587eb2c8d8cd7b132ffe27d
masresha-y <heloomas2021@gmail.com>
2023-11-08 21:03:01 +0300

    some minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,10 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckOutComponent } from './check-out/check-out.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -22,13 +27,29 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
     ShoppingCartComponent,
     CheckOutComponent,
     OrderSuccessComponent,
-    MyOrdersComponent
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp({ "projectId": "oshop-45032", "appId": "1:659653208630:web:89e1127ffbbf4655400697", "storageBucket": "oshop-45032.appspot.com", "apiKey": "AIzaSyBFVbjSfrqDl_DqGnYaPv9K-NCZdcWJCZY", "authDomain": "oshop-45032.firebaseapp.com", "messagingSenderId": "659653208630" })),
-    provideAuth(() => getAuth())
+    provideAuth(() => getAuth()),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent }, { path: 'admin/products', component: AdminProductsComponent },
+
+      { path: 'products', component: ProductsComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'check-out', component: CheckOutComponent },
+      { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'my/orders', component: MyOrdersComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'admin/products', component: AdminProductsComponent },
+      { path: 'admin/orders', component: AdminOrdersComponent },
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
